97c52b6512ffb52502cf15e4ed9d807b58be85dd
Djesse-Bruijn <74538187+Djesse-Bruijn@users.noreply.github.com>
2023-01-23 11:26:14 +0100

    created post table correctly

--- database/migrations/2023_01_23_102418_create__post_table.php
+++ database/migrations/2023_01_23_102418_create__post_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('tasks', function (Blueprint $table) {
+        Schema::create('Post', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->string('text');
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('Posts');
+        Schema::dropIfExists('Post');
     }
 };
