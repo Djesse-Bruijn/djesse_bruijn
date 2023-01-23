fddec7951bc46e40a865062c1c73bf3acb61686a
Djesse-Bruijn <74538187+Djesse-Bruijn@users.noreply.github.com>
2023-01-23 10:47:45 +0100

    corrected my new mirgation

--- database/migrations/2023_01_23_092704_create__post_table.php
+++ database/migrations/2023_01_23_092704_create__post_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->title();
-            $table->text();
-            $table->author();
+            $table->string('title');
+            $table->string('text');
+            $table->string('author');
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Route::get('login', [CustomAuthController::class, 'index'])->name('login');
 Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom');
 Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
 Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom');
-Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout'); 
+Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
